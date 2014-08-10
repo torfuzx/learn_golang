0ea23648580c0e8660af0d37fd3f4cb1769136d5
Kurt Zhong <kurtzhong520@gmail.com>
2014-08-10 18:55:53 +0800

    Improve the cogrep

--- demo_cogrep/cogrep.go
+++ demo_cogrep/cogrep.go
@@ -1,35 +1,121 @@
 /**
+ *
  * An example to demonstrate cocurrent programming with go.
+ *
+ *
  */
 
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
-	"runtime"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
-	"log"
-	"bufio"
-	"io"
+	"runtime"
 )
 
-type Result struct {
-	filename 	string
-	lino	 	int
-	line		string
-}
+var (
+	pattern   string // the pattern to grep
+	path      string // the path, either file or directory
+	workerNum = runtime.NumCPU()
+)
 
 type Job struct {
 	filename string
-	results chan<- Result
+	results  chan<- Result
+}
+
+// Type used to represent the yield final type
+type Result struct {
+	filename string // the name of file that the pattern appears in
+	lino     int    // the line number of the file
+	line     string // the content of that file
+}
+
+func init() {
+	parseCommandLine()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// Set a logger file
+	file, err := os.OpenFile("cogrep.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error open log file: %v", err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Println("This is a test entry")
+}
+
+func main() {
+	// buffered channel of type Job
+	chanJob := make(chan Job, workerNum)
+	// buffered channel of type Result
+	chanResult := make(chan Result, 1000)
+	// buffered channel of type struct{}, buffer size is the number of cpu cores
+	chanDone := make(chan struct{}, workerNum)
+
+	// use a worker goroutine for generating jobs
+	// it acts as the producer in the producer-consumer modle
+	// here a job represents a file's path, that will iterated under the path
+	go produceJobs(path, chanJob, chanResult)
+
+	// use separate worker goroutine to process the jobs
+	// these series of goroutines act as the consumers in the producer-consumer
+	// model
+	for i := 0; i < workerNum; i++ {
+		go consumeJobs(pattern, chanJob, chanDone)
+	}
+
+	// another goroutine to wait for completed jobs
+	// here another goroutine for receiving the consumers' outcome, like a boss
+
+	// this goroutine wait the completion of each doJobs goroutine,
+	// the destination channel is put before the source channel
+	// here the done channel is set to receive only
+	go func(done <-chan struct{}, result chan Result) {
+		for i := 0; i < workerNum; i++ {
+			<-done
+		}
+		close(result)
+	}(chanDone, chanResult)
+
+	// process the results in the main goroutine
+	for result := range chanResult {
+		fmt.Printf("%s:%d:%s\n", result.filename, result.lino, result.line)
+	}
+}
+
+func parseCommandLine() {
+	flag.StringVar(&pattern, "e", "", "The search pattern")
+	flag.StringVar(&path, "p", "", "The file to grep against")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+
+	flag.Parse()
+
+	if flag.NFlag() < 2 {
+		flag.Usage()
+	}
+
+	if _, err := regexp.Compile(pattern); err != nil {
+		log.Fatal("invalid pattern: %s\n", err)
+	}
 }
 
 // this is where the hard work is taken, it's invoked by a doJob goroutine
 // it will send the matched file name, line number, and the contents of the
 // matched line to the Result channel. which is waited by main goroutine
-func (job Job) Do(lineRx *regexp.Regexp) {
+func (job Job) Do(pattern string) {
+
 	file, err := os.Open(job.filename)
 	if err != nil {
 		log.Printf("error: %s\n", err)
@@ -40,11 +126,13 @@ func (job Job) Do(lineRx *regexp.Regexp) {
 	// the surrouding function returns
 	defer file.Close()
 
+	regx, err := regexp.Compile(pattern)
+
 	// the bufio.NewReader will return a new Reader with default buffer size
 	reader := bufio.NewReader(file)
-	for lino := 1; ;lino ++ {
+	for lino := 1; ; lino++ {
 		line, err := reader.ReadBytes('\n')
-		if lineRx.Match(line) {
+		if regx.Match(line) {
 			job.results <- Result{job.filename, lino, string(line)}
 		}
 
@@ -57,124 +145,47 @@ func (job Job) Do(lineRx *regexp.Regexp) {
 	}
 }
 
-func main() {
-	// use as many cores as possible
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	// provide help message when asked by the user
-	if len(os.Args) < 3 || os.Args[1] == "-h" || os.Args[1] == "-help" {
-		fmt.Printf("usage: %s <regexp> <files>\n", filepath.Base(os.Args[0]))
-		os.Exit(1)
-	}
-
-	// the regexp.Compile() returns a *regexp.Regexp and nil if compiled ok
-	if lineRx, err := regexp.Compile(os.Args[1]); err != nil {
-		log.Fatal("invalid regexp: %s\n", err)
-	} else {
-		grep(lineRx, commandLineFiles(os.Args[2:]))
-	}
-}
-
-func commandLineFiles(files []string) []string {
-	if runtime.GOOS == "windows" {
-		args := make([]string, 0, len(files))
-		for _,name := range (files) {
-			if matches, err := filepath.Glob(name); err != nil {
-				args = append(args, name)
-			} else if  matches != nil {
-				args = append(args, matches...)	// an example of append one slice to another slice
-			}
-		}
-		return args
-	}
-	return files
-}
-
-var workers = runtime.NumCPU()
-
-func grep(lineRx *regexp.Regexp, filenames []string) {
-	// the lineRx will be shared among the worker goroutines, although we
-	// should always assume that the vlaue pointed by a shared pointer is not
-	// thread-safe, the go officdoc says it's thread safe, so we could safely use
-	// this pointer among all the goroitines
-	jobs 	:= make(chan Job, workers)							// asym channel of type Job, buffer size is the number of cpu cores
-	results := make(chan Result, minimum(1000, len(filenames)))	// asym channel of type Result
-	done 	:= make(chan struct {}, workers)					// asym channel of type struct{}, buffer size is the number of cpu cores
-
-	// use a worker goroutine for scheduling/enqueing the jobs
-	go addJobs(jobs, filenames, results)
-
-	// use separate worker goroutines to process the jobs
-	for i := 0; i < workers; i++ {
-		go doJobs(done, lineRx, jobs)
-	}
-
-	// another goroutine to wait for complted jobs
-	go awaitCompletion(done, results)
-
-	// process the results in the main go routine
-	processResults(results)
-}
-
-
 // this goroutine will sending all the jobs to the jobs channel and close it
 // after sending.
-//
-// the jobs channel can be only to send Job
-// the result channel can be used only to send Result
-
-// Note: the <- operator specifies the channel direction:
-// 1. chan<- TYPE means can be only used to send objects of type TYPE
-// 2. <-chan TYPE means can be only used to recv objects of type TYPE
-func addJobs(jobs chan<- Job, filenames []string, results chan<- Result) {
-	for _, filename := range filenames {
-		jobs <- Job{filename, results}
+// - the jobs channel can be only to send Job
+// - the result channel can be used only to send Result
+func produceJobs(path string, chJob chan<- Job, chRes chan<- Result) {
+	// iterate the directory, and sending each
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
 	}
-	// after the jobs channel sending all the jobs(file), close the channel
-	close(jobs)
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		// iterate the directory
+		filepath.Walk(path, func(p string, f os.FileInfo, err error) error {
+			chJob <- Job{p, chRes}
+			return nil
+		})
+
+	case mode.IsRegular():
+		chJob <- Job{path, chRes}
+	}
+
+	// close channel after sending
+	close(chJob)
 }
 
 // This goroutine will receive the jobs from the jobs channel and invoke the
 // the job's do method to process method.
-//
 // this goroutine is blocking and after the it handles all the jobs dispatched
 // this will send a succuess signal to the done channel.
-//
 // do job, will run in a separate go routine
-//
-func doJobs(done chan <- struct{}, lineRx *regexp.Regexp, jobs chan Job) {
-	for job := range jobs {
-		job.Do(lineRx)
+func consumeJobs(pattern string, chJob <-chan Job, chDone chan<- struct{}) {
+	for job := range chJob {
+		job.Do(pattern)
 	}
-	done <- struct{}{}// this is an send statement, the done channel is a
-					  // send-only channel, so we can send things into it
+	chDone <- struct{}{}
 }
-
-// this goroutine wait the completion of each doJobs goroutine,
-
-// the destination channel is put before the source channel
-// here the done channel is set to receive only
-func awaitCompletion(done <-chan struct{}, result chan Result) {
-	for i := 0; i w< workers; i++ {
-		<-done
-	}
-	close(result)
-}
-
-// just print the matched message
-func processResults(results <-chan Result) {
-	for result := range results {
-		fmt.Printf("%s:%d:%s\n", result.filename, result.lino, result.line)
-	}
-}
-
-func minimum(x int, ys ...int) int {
-	for _, y := range ys {
-		if y < x {
-			x = y
-		}
-	}
-	return x
-}
-
-
