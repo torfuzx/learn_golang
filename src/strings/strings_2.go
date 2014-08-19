02e160c119e709e7721e61ee2089f9ed4b7b230a
Kurt Zhong <kurtzhong520@gmail.com>
2014-08-19 09:14:02 +0800

    update some directory names

---
+++ src/strings/strings_2.go
@@ -0,0 +1,20 @@
+/*
+
+Strings are not required to be UTF-8.Go source code is required to be UTF-8.
+
+In short, there are three kinds of strings:
+- the substring of the source that lexes into a string literal
+- a string literal
+- a value of type string
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+}
