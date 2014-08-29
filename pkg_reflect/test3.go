65e9da3550d739ba2f0b64dc7eef8dcf39b782d9
Kurt Zhong <kurtzhong520@gmail.com>
2014-08-29 15:13:04 +0800

    add another reflect example

--- pkg_reflect/test3.go
+++ pkg_reflect/test3.go
@@ -1,3 +1,14 @@
+/*
+
+- ValueOf() returns a new value initialized to the concrete value stored in the 
+interface i.
+- Value.FieldByName() returns the struct field with the given name
+- Value.Int() returns underlying vlaue, as a int64
+- Value.Elem() returns the value that the interface v contains or that the 
+pointer v points to.
+
+*/
+
 package main
 
 import (
@@ -12,13 +23,13 @@ type S struct {
 
 func main() {
 	s := S{1}
-	
-	// get 
+
+	// get
 	immutable := reflect.ValueOf(s)
 	val := immutable.FieldByName("N").Int()
 	fmt.Printf("n=%d\n", val)
-	
-	// set 
+
+	// set
 	mutable := reflect.ValueOf(&s).Elem()
 	mutable.FieldByName("N").SetInt(7)
 	fmt.Printf("n=%d\n", s.N)
