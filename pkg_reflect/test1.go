daae14a2aea039a2377534abe27e1bb03e54317e
Kurt Zhong <kurtzhong520@gmail.com>
2014-08-31 13:44:34 +0800

    update

--- pkg_reflect/test1.go
+++ pkg_reflect/test1.go
@@ -5,18 +5,24 @@ import (
 	"reflect"
 )
 
-func main() {
-	type S struct {
-		F string `name:"Kurt Zhong" email: "kurtzhong520@gmail.com"`
-	}
+type S struct {
+	// Please note that:
+	// 1. The field and field value shouldn't be separated by any
+	// space.
+	// 2. The field value could be closely followd by the field name without
+	// space in the middle.
+	// 3. The field value is quoted with double-quotes.
+	F string `name:"Kurt Zhong"email:"kurtzhong520@gmail.com"`
+}
 
+func main() {
 	s := S{}
-	//
 	st := reflect.TypeOf(s)
 	fmt.Println(st)
 
 	// returns the first field of struct
 	field := st.Field(0)
 
-	fmt.Println(field.Tag.Get("emailz"))
+	fmt.Println(field.Tag.Get("name"))
+	fmt.Println(field.Tag.Get("email"))
 }
--- pkg_reflect/test2.go
+++ pkg_reflect/test2.go
@@ -18,24 +18,24 @@ func main() {
 	// invoked, reflect turns the arguments into values, calls swap, and then
 	// turns swap's result slice into the values returned by the new function.
 	makeSwap := func(fptr interface{}) {
-		// fptr is a pointerr to a function. Obtain the function value itself, 
+		// fptr is a pointerr to a function. Obtain the function value itself,
 		// (like nil) as a reflect.Value, so that we can query its type and then
 		// set the value.
 		fn := reflect.ValueOf(fptr).Elem()
-		
+
 		// make a fucntion of the right type
 		v := reflect.MakeFunc(fn.Type(), swap)
-		
+
 		// assign it to the value fn represents
 		fn.Set(v)
 	}
-	
+
 	// make and call a swap function for  ints
 	var intSwap func(int, int) (int, int)
 	makeSwap(&intSwap)
 	fmt.Println(intSwap(0, 1))
 
-	// make and call a swap function for float64s	
+	// make and call a swap function for float64s
 	var floatSwap func(float64, float64) (float64, float64)
 	makeSwap(&floatSwap)
 	fmt.Println(floatSwap(2.72, 3.14))
